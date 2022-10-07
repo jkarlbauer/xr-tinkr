b65ccde58473f0b4ef47791c7c7e07beb1610032
Julian Karlbauer <julian.karlbauer@gmx.de>
2022-10-07 14:09:59 +0200

    feat: display now follows head movements

---
+++ Assets/Modules/UI/Debug/FollowHeadPose.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xrtinkr.UI.Debug
+{
+    public class FollowHeadPose : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Transform head;
+
+  
+        void Update()
+        {
+            Vector3 verticalOffset = new Vector3(0, -0.5f, 0);
+            Vector3 positionOffsetFromHead = head.position + head.transform.forward + verticalOffset;
+            transform.position = Vector3.Lerp(transform.position, positionOffsetFromHead, 0.08f);
+            Vector3 fromHeadToHere = transform.position - head.position;
+            transform.forward = Vector3.Lerp(transform.forward, fromHeadToHere, 0.08f);
+        }
+    }
+}
+
