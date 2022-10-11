c4b4982dbba6772af7b61b277b893f1dc808706b
Julian Karlbauer <julian.karlbauer@gmx.de>
2022-10-11 12:44:29 +0200

    working on quest file loading

--- Assets/Modules/DataLoading/Scritps/LoadGltfData.cs
+++ Assets/Modules/DataLoading/Scritps/LoadGltfData.cs
@@ -1,22 +1,48 @@
 
 using UnityEngine;
 using Siccity.GLTFUtility;
-using System.IO;
 
 public class LoadGltfData : MonoBehaviour
 {
-    private string _dataPath = "sophies_tiny_house.glb";
-
     private void OnEnable()
     {    
-        _dataPath = Application.persistentDataPath + "sophies_tiny_house.glb";
+        string _dataPath = Application.persistentDataPath + "/sample.glb";
+
+        _dataPath = "sophies_tiny_house.glb";
+
         ImportGLTF(_dataPath);
     }
 
     void ImportGLTF(string filepath)
     {
         GameObject result = Importer.LoadFromFile(filepath);
+        result.transform.localScale /= 10;
+        Debug.Log("Loaded GLB file");
+        CenterObjectInScene(result);
     }
-    
 
+    private void CenterObjectInScene(GameObject go)
+    {
+        MeshFilter[] meshfilters = go.GetComponentsInChildren<MeshFilter>();
+
+        Vector3 centerOfMass = Vector3.zero;
+        int cnt = 0;
+        foreach(MeshFilter mf in meshfilters)
+        {
+            centerOfMass += mf.sharedMesh.bounds.center;
+            cnt++;
+        }
+
+        if(cnt > 0)
+        {
+            centerOfMass /= cnt;
+        }
+
+        Debug.Log("Center of mass: " + centerOfMass);
+        GameObject parent = new GameObject("parent");
+        parent.transform.position = centerOfMass;
+        go.transform.parent = parent.transform;
+        parent.transform.position = Vector3.zero;
+
+    }
 }
