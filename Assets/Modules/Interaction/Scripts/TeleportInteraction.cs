da476bdff5661b65a1e936d4e3f59e67ed0c1ad0
Julian Karlbauer <julian.karlbauer@gmx.de>
2022-10-13 14:36:31 +0200

    added Shader Graph

--- Assets/Modules/Interaction/Scripts/TeleportInteraction.cs
+++ Assets/Modules/Interaction/Scripts/TeleportInteraction.cs
@@ -1,49 +1,66 @@
 using UnityEngine;
 using Oculus.Interaction;
 
-[RequireComponent(typeof(RayInteractor))]
-[RequireComponent(typeof(LineRenderer))]
-public class TeleportInteraction : MonoBehaviour
+namespace Xrtinkr.Interaction
 {
-    [SerializeField]
-    private GameObject _reticle;
-
-    [SerializeField]
-    private GameObject _OVRCameraRig;
-
-    private RayInteractor _rayInteractor;
-
-    private LineRenderer lineRenderer;
-
-    private void OnEnable()
+    [RequireComponent(typeof(RayInteractor))]
+    [RequireComponent(typeof(LineRenderer))]
+    public class TeleportInteraction : MonoBehaviour
     {
-        _rayInteractor = GetComponent<RayInteractor>();
-        _rayInteractor.WhenStateChanged += ProcessState;
-        _reticle = Instantiate(_reticle);
-        lineRenderer = GetComponent<LineRenderer>();
-    }
+        [SerializeField]
+        private GameObject _reticle;
 
-    private void ProcessState(InteractorStateChangeArgs obj)
-    {
-        if(obj.NewState == InteractorState.Select)
+        [SerializeField]
+        private GameObject _OVRCameraRig;
+
+        private RayInteractor _rayInteractor;
+
+        private LineRenderer lineRenderer;
+
+        private void OnEnable()
         {
-            _OVRCameraRig.transform.position = _reticle.transform.position;
-        }
-    }
+            _rayInteractor = GetComponent<RayInteractor>();
+            _rayInteractor.WhenStateChanged += ProcessState;
+            _reticle = Instantiate(_reticle);
+            _reticle.SetActive(false);
+            lineRenderer = GetComponent<LineRenderer>();
 
-    private void Update()
-    {
-        lineRenderer.SetPositions(new Vector3[] { _rayInteractor.Ray.origin, _rayInteractor.Ray.origin + _rayInteractor.Ray.direction });
-
-        RaycastHit hit;
-        if (Physics.Raycast(_rayInteractor.Ray.origin, _rayInteractor.Ray.direction, out hit, Mathf.Infinity, LayerMask.GetMask("TeleportTarget")))
-        {
-            _reticle.transform.position = hit.point;     
         }
 
- 
+        private void ProcessState(InteractorStateChangeArgs obj)
+        {
+
+            Debug.Log(obj.PreviousState);
+            Debug.Log(obj.NewState);
+            if (obj.PreviousState == InteractorState.Hover && obj.NewState == InteractorState.Select)
+            {
+                ShowReticle();
+            }
+
+            if (obj.PreviousState == InteractorState.Select && obj.NewState == InteractorState.Hover)
+            {
+                HideReticle();
+                Teleport();
+            }
+        }
+
+        private void Update()
+        {
+            lineRenderer.SetPositions(new Vector3[] { _rayInteractor.Ray.origin, _rayInteractor.Ray.origin + _rayInteractor.Ray.direction });
+
+            RaycastHit hit;
+            if (Physics.Raycast(_rayInteractor.Ray.origin, _rayInteractor.Ray.direction, out hit, Mathf.Infinity, LayerMask.GetMask("TeleportTarget")))
+            {
+                _reticle.transform.position = hit.point;
+            }
+
+
+        }
+
+        private void ShowReticle() => _reticle.SetActive(true);
+        private void HideReticle() => _reticle.SetActive(false);
+        private void Teleport() => _OVRCameraRig.transform.position = _reticle.transform.position;
+
     }
-
-
 
 }
